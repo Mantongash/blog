81929c2351830c1269ba007bfc54716840938157
Mantongash <you@example.com>
2020-01-19 03:42:38 +0300

    Changed home.html to index.html, I had made a mistake there

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # routes
 @app.route("/")
 def home():
-  return render_template("home.html", title="Home")
+  return render_template("index.html", title="Home")
 
 if __name__ == "__main__":
   app.run(debug=True)
