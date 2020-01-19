a9866183af2ce8c27b9b25fa5c4ed8db9f51f881
Mantongash <you@example.com>
2020-01-19 03:41:07 +0300

    Created a route to the home page

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# routes
+@app.route("/")
+def home():
+  return render_template("home.html", title="Home")
+
 if __name__ == "__main__":
   app.run(debug=True)
