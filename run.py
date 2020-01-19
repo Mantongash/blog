2490a842118926112aaa9af4bc79f2b349b7a356
Mantongash <you@example.com>
2020-01-19 07:16:04 +0300

    Added a route for the about page

--- run.py
+++ run.py
@@ -6,5 +6,10 @@ from app import app
 def home():
   return render_template("index.html", title="Home")
 
+#About route
+@app.route("/about")
+def about():
+  return render_template("about.html", title="About")
+
 if __name__ == "__main__":
   app.run(debug=True)
